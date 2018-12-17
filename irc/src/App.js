fa50e3301ad31284a9bacf8ed4fd4283ee42acdd
Leon <antoine.leon@epitech.eu>
2018-12-17 14:34:49 +0100

    push

--- irc/src/App.js
+++ irc/src/App.js
@@ -1,24 +1,120 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import io from 'socket.io-client';
-
+import React, { Component } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import "bootstrap/dist/css/bootstrap.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 class App extends Component {
-
-  componentDidMount() {
-    this.socket = io("localhost:3001");
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      submit: false,
+      send: "",
+      messages: [],
+      message: "",
+      user: []
+    };
+    this.socket = io("localhost:3000");
+    this.socket.on("receive-message", msg => {
+      this.setState({ messages: [...this.state.messages, msg] });
+    });
   }
   componentWillUnmount() {
     this.socket.disconnect();
   }
-  
+  componentDidMount() {}
+
+  MessageSet = e => {
+    this.setState({ message: this.state.username + "  :  " + e.target.value });
+  };
+  SendMessage = e => {
+    e.preventDefault();
+    this.socket.emit("new-message", this.state.message);
+    e.target.reset();
+    toast.info("Message send", {
+      position: "top-center",
+      autoClose: 1000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true
+    });
+  };
+  submit = e => {
+    e.preventDefault();
+    this.setState({ submitted: true });
+    toast("Welcome " + this.state.username + " !", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true
+    });
+    this.setState({
+      messages: [...this.state.messages, this.state.username + " connected !"]
+    });
+  };
   render() {
-    return (
-<h1>h</h1>
+    var self = this;
+    var i = 0;
+    var message = self.state.messages.map(function(msg) {
+      return (
+        <div className="divmessage">
+          <li className="textmessage" key={i++}>
+            {msg}
+          </li>
+        </div>
       );
+    });
+    if (this.state.submitted) {
+      return (
+        <div>
+          <h1>React Chat</h1>
+          <ul>{message}</ul>
+          <ToastContainer />
+          <div>
+            <form onSubmit={this.SendMessage}>
+              <div>
+                <input
+                  id="message"
+                  type="text"
+                  onChange={this.MessageSet}
+                  placeholder="Enter a message..."
+                  required
+                />
+              </div>
+              <input
+                type="submit"
+                value="Submit"
+                id="smessage"
+                onSubmit={this.SendMessage}
+              />
+            </form>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>React Chat</h1>
+          <div>
+            <input
+              type="text"
+              onChange={e => {
+                this.setState({ username: e.target.value });
+              }}
+              placeholder="Enter a username..."
+              required
+            />
+          </div>
+          <input type="submit" value="Submit" onClick={this.submit} />
+        </div>
+      );
+    }
   }
 }
 
 export default App;
-  
--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
-
-
-io.on('connection', function (socket) {
-        console.log('a user connected');
+io.on("connection", socket => {
+  console.log("New client connected");
+  socket.on("new-message", function(msg) {
+    console.log(msg);
+    io.emit("receive-message", msg);
+  });
 });
 
-http.listen(3000, function () {
-        console.log('listening on *:3000');
+http.listen(3000, function() {
+  console.log("listening on *:3000");
 });
